956b0e69d57dd1936f59260ee36465fc14ebd7e7
Priboy313 <Priboy313@yandex.ru>
2025-11-16 19:55:29 +0300

    add zoom, add physic materials

--- Assets/_Project/Scripts/CameraHandler.cs
+++ Assets/_Project/Scripts/CameraHandler.cs
@@ -3,10 +3,70 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraHandler : MonoBehaviour
 {
+	[SerializeField] InputReader _input;
+
+	[Header("Following")]
 	[SerializeField] private Transform _followedObject;
 	[SerializeField] private float _speed = 2f;
-	
-	void LateUpdate()
+
+	[Header("Zoom")]
+	[SerializeField] private float _zoomMin = 4f;
+	[SerializeField] private float _zoomMax = 10f;
+	[SerializeField] private float _zoomSpeed = 0.2f;
+	[SerializeField] private float _zoomCurrent = 5f;
+
+	private Camera _camera;
+
+    private void OnValidate()
+    {
+		if (_zoomCurrent < _zoomMin)
+		{
+			_zoomCurrent = _zoomMin;
+		}
+
+		if (_zoomCurrent > _zoomMax)
+		{
+			_zoomCurrent = _zoomMax;
+		}
+    }
+
+    private void Awake()
+    {
+		if (_input == null)
+		{
+            _input = FindObjectOfType<InputReader>();
+
+			if (_input == null)
+			{
+				Debug.LogError("InputReader not set!");
+				enabled = false;
+			}
+		}
+
+		_camera = GetComponent<Camera>();
+
+        if (!_camera.orthographic)
+        {
+            Debug.LogWarning("Камера не является ортографической! Масштабирование может работать не так, как ожидается.", this);
+        }
+    }
+
+    private void OnEnable()
+    {
+		_input.ActionZoomChange += OnZoomChange;
+    }
+
+    private void OnDisable()
+    {
+		_input.ActionZoomChange -= OnZoomChange;
+    }
+
+    private void Start()
+    {
+		_camera.orthographicSize = _zoomCurrent;
+    }
+
+    void LateUpdate()
 	{
 		if (_followedObject == null)
 		{
@@ -18,5 +78,25 @@ public class CameraHandler : MonoBehaviour
 			new Vector3(_followedObject.position.x, _followedObject.position.y, transform.position.z), 
 			_speed * Time.deltaTime
 		);
+	}
+
+	private void OnZoomChange(float change)
+	{
+		float sizeCurrent = _camera.orthographicSize;
+
+        sizeCurrent += change * _zoomSpeed;
+
+		if (sizeCurrent < _zoomMin)
+		{
+			sizeCurrent = _zoomMin;
+		}
+
+		if (sizeCurrent > _zoomMax)
+		{
+			sizeCurrent = _zoomMax;
+		}
+
+		_zoomCurrent = sizeCurrent;
+		_camera.orthographicSize = _zoomCurrent;
 	}
 }
--- Assets/_Project/Scripts/System/InputReader.cs
+++ Assets/_Project/Scripts/System/InputReader.cs
@@ -3,16 +3,52 @@ using UnityEngine;
 
 public class InputReader : MonoBehaviour 
 {
+    private const KeyCode ZoomIn = KeyCode.KeypadPlus;
+    private const KeyCode ZoomOut = KeyCode.KeypadMinus;
+    private const float ZoomKeyboardSpeed = 1f;
+    private const bool IsZoomInvert = false;
+
+    private const string AxisScrollWheel = "Mouse ScrollWheel";
+    private const string AxisHorizontal = "Horizontal";
+    private const string ButtonJumpName = "Jump";
+
     public event Action<float> ActionMove;
     public event Action ActionJump;
+    public event Action<float> ActionZoomChange;
 
     private void Update()
     {
-        ActionMove?.Invoke(Input.GetAxisRaw("Horizontal"));
+        ObserveKeyboard();
+        ObserveMouse();
+    }
 
-        if (Input.GetButtonDown("Jump"))
+    private void ObserveKeyboard()
+    {
+        ActionMove?.Invoke(Input.GetAxisRaw(AxisHorizontal));
+
+        if (Input.GetButtonDown(ButtonJumpName))
         {
             ActionJump?.Invoke();
+        }
+
+        if (Input.GetKeyDown(ZoomIn))
+        {
+            ActionZoomChange?.Invoke(IsZoomInvert ? ZoomKeyboardSpeed : -ZoomKeyboardSpeed);
+        }
+
+        if (Input.GetKeyDown(ZoomOut))
+        {
+            ActionZoomChange?.Invoke(IsZoomInvert ? -ZoomKeyboardSpeed : ZoomKeyboardSpeed);
+        }
+    }
+
+    private void ObserveMouse()
+    {
+        float scrollWheel = Input.GetAxisRaw(AxisScrollWheel);
+
+        if (scrollWheel != 0)
+        {
+            ActionZoomChange?.Invoke(IsZoomInvert ? scrollWheel : -scrollWheel);
         }
     }
 }
