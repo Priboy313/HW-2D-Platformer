9ae8739fc72b501d803f3ac8b0c0a484622b48cc
Priboy313 <Priboy313@yandex.ru>
2025-11-16 20:12:42 +0300

    OnGroundTrigger fix

--- Assets/_Project/Scripts/OnGroundTrigger.cs
+++ Assets/_Project/Scripts/OnGroundTrigger.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class OnGroundTrigger : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+
 	public event Action<bool> ActionOnGround;
 
 	private void Awake()
 	{
+        bool hasErrors = false;
+
 		if (GetComponent<Collider2D>().isTrigger == false)
 		{
 			Debug.LogError("Trigger not set!");
-			enabled = false;
+            hasErrors = true; ;
 		}
-	}
+
+        _rigidbody = GetComponentInParent<Rigidbody2D>();
+
+        if (_rigidbody == null)
+        {
+            Debug.LogError("Parent Rigidbody2D not found!");
+            hasErrors = true;
+        }
+
+        enabled = !hasErrors;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<SolidSurface>(out _))
+        if (_rigidbody.velocity.y <= 0 && collision.TryGetComponent<SolidSurface>(out _))
         {
             ActionOnGround?.Invoke(true);
         }
